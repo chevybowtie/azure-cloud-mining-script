8410d49c12ad8f372cfacf1a431b96375c1dd149
chevybowtie <paul+github@paulsturm.net>
2021-05-15 01:13:10 -0500

    Update setup_vm_nim.sh

--- azure_script/setup_vm_nim.sh
+++ azure_script/setup_vm_nim.sh
@@ -4,10 +4,10 @@ sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-wget https://github.com/Beeppool/miner/releases/download/0.3.4/beepminer-0.3.4.zip
-unzip beepminer-0.3.4.zip
+wget https://github.com/Beeppool/miner/releases/download/0.6.1/beepminer-0.6.1.zip
+unzip beepminer-0.6.1.zip
 mkdir donation
-cp -r beepminer-0.3.4/* donation
+cp -r beepminer-0.6.1/* donation
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
@@ -15,7 +15,7 @@ let donation*=10
 
 for i in `seq 1 4`;
 do
-    cd beepminer-0.3.4
+    cd beepminer-0.6.1/
     sudo timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id
     cd ..
     if [ $donation -gt 0 ]
